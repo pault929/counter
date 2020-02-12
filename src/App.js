304a9f40aa7772540d97f7572793e002d209a0ab
unknown <paultoro101@gmail.com>
2020-02-11 21:48:37 -0500

    Added dingle and double button

--- src/App.js
+++ src/App.js
@@ -34,9 +34,10 @@ reset =() => {
         <div className="navbar">Counter</div>
         <div className="counter">
           <h1>{this.state.count}</h1>
+          <button type="button" onClick={this.reset}>Reset Count</button>
           <button type="button" onClick={this.increment}>Increment</button>
           <button type="button" onClick={this.decrement}>Decrement</button>
-          <button type="button" onClick={this.reset}>Reset Count</button>
+          <button type="button" onClick={this.decrement}>Single count</button>
         </div>
       </div>
     )
